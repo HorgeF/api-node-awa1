8f935984ec519aa1d89b2bf633993944ebedce8e
logisticals.devteam <logisticals.devteam@outlook.com>
2024-05-08 09:17:34 -0500

    222222

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-//const mysql = require('mysql')
-const mysql = require('mysql2');
+const mysql = require('mysql')
+//const mysql = require('mysql2');
 const bodyparser = require('body-parser')
 
 const app = express()
