fe921f6e33b3ba58c209844f5212ff4cefb2d255
Miguel Yance <miguelygonzales@gmail.com>
2024-05-11 20:08:20 -0500

    CRUD Y LOGIN USER

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.listen(PUERTO, () => {
             if(error) return console.log(error.message)
             
             if(resultado.length > 0){
-                res.json(resultado[0])
+                res.json(resultado)
             }
         })
     })
@@ -130,85 +130,151 @@ app.listen(PUERTO, () => {
 
     app.post('/RegistrarUsuario', (req, res) => {
         const sp_nombre = 'REGISTRAR_USUARIO';
-        const sp_parametro = req.body.id; // Obtiene el valor del parámetro 'id' del cuerpo de la solicitud
+        
+        const sp_parametro1 = req.body.USUARIO;
+        const sp_parametro2 = req.body.CONTRASENIA;
+        const sp_parametro3 = req.body.USU_NOMBRE;
+        const sp_parametro4 = req.body.USU_SEXO;
+        const sp_parametro5 = req.body.USU_FECHA_NACIMIENTO;
+        const sp_parametro6 = req.body.USU_EDAD;
+        const sp_parametro7 = req.body.USU_PESO;
+        const sp_parametro8 = req.body.USU_TALLA;
+        const sp_parametro9 = req.body.USU_CORREO;
+        const sp_parametro10 = req.body.USU_CELULAR;
     
         // Llama al procedimiento almacenado con el parámetro 'id'
-        const query = `CALL ${sp_nombre}(${sp_parametro})`;
-    
-        connection.query(query, (error, resultado) => {
+        const query = `CALL ${sp_nombre}(?,?,?,?,?,?,?,?,?,?,@RESULTADO)`;    
+        connection.query(query,[sp_parametro1,sp_parametro2,
+                                sp_parametro3,sp_parametro4,
+                                sp_parametro5,sp_parametro6,
+                                sp_parametro7,sp_parametro8,
+                                sp_parametro9,sp_parametro10], (error, resultado) => {
             if (error) {
                 console.error('Error al llamar al procedimiento almacenado:', error.message);
                 return res.status(500).json({ error: 'Error interno del servidor' });
             }
-    
-            if (resultado && resultado.length > 0) {
-                res.json(resultado[0]);
-            } else {
-                res.json({ mensaje: 'No se encontraron resultados' });
-            }
+
+            // Fetch the output parameter value by executing another query
+            connection.query('SELECT @RESULTADO as outputValue', (err, result) => {
+                if (err) {
+                    console.error('Error fetching output parameter:', err.stack);
+                    return res.status(500).send('Error fetching data from the database');
+                }
+                
+                if (result && result.length > 0) {
+                    res.json(result[0]);
+                } else {
+                    res.json({ mensaje: 'Ocurrió un error, no se pudo registrar, intente nuevamente' });
+                } 
+            });
+
         });
     });
 
     app.post('/EditarUsuario', (req, res) => {
         const sp_nombre = 'EDITAR_USUARIO';
-        const sp_parametro = req.body.id; // Obtiene el valor del parámetro 'id' del cuerpo de la solicitud
+        
+        const sp_parametro = req.body.id;
+        const sp_parametro1 = req.body.USUARIO;
+        const sp_parametro2 = req.body.CONTRASENIA;
+        const sp_parametro3 = req.body.USU_NOMBRE;
+        const sp_parametro4 = req.body.USU_SEXO;
+        const sp_parametro5 = req.body.USU_FECHA_NACIMIENTO;
+        const sp_parametro6 = req.body.USU_EDAD;
+        const sp_parametro7 = req.body.USU_PESO;
+        const sp_parametro8 = req.body.USU_TALLA;
+        const sp_parametro9 = req.body.USU_CORREO;
+        const sp_parametro10 = req.body.USU_CELULAR;
     
         // Llama al procedimiento almacenado con el parámetro 'id'
-        const query = `CALL ${sp_nombre}(${sp_parametro})`;
+        const query = `CALL ${sp_nombre}(?,?,?,?,?,?,?,?,?,?,?,@RESULTADO)`;
     
-        connection.query(query, (error, resultado) => {
+        connection.query(query,[sp_parametro,sp_parametro1,
+                                sp_parametro2,sp_parametro3,
+                                sp_parametro4,sp_parametro5,
+                                sp_parametro6,sp_parametro7,
+                                sp_parametro8,sp_parametro9,
+                                sp_parametro10], (error, resultado) => {
             if (error) {
                 console.error('Error al llamar al procedimiento almacenado:', error.message);
                 return res.status(500).json({ error: 'Error interno del servidor' });
             }
     
-            if (resultado && resultado.length > 0) {
-                res.json(resultado[0]);
-            } else {
-                res.json({ mensaje: 'No se encontraron resultados' });
-            }
+            // Fetch the output parameter value by executing another query
+            connection.query('SELECT @RESULTADO as outputValue', (err, result) => {
+                if (err) {
+                    console.error('Error fetching output parameter:', err.stack);
+                    return res.status(500).send('Error fetching data from the database');
+                }
+                
+                if (result && result.length > 0) {
+                    res.json(result[0]);
+                } else {
+                    res.json({ mensaje: 'Ocurrió un error, no se pudo actualizar intente nuevamente' });
+                } 
+            });
+
         });
     });
 
     app.post('/LoginUsuario', (req, res) => {
         const sp_nombre = 'LOGIN_USUARIO';
-        const sp_parametro = req.body.id; // Obtiene el valor del parámetro 'id' del cuerpo de la solicitud
+        const sp_parametro1 = req.body.USUARIO;
+        const sp_parametro2 = req.body.CONTRASENIA; // Obtiene el valor del parámetro 'id' del cuerpo de la solicitud
     
         // Llama al procedimiento almacenado con el parámetro 'id'
-        const query = `CALL ${sp_nombre}(${sp_parametro})`;
+        const query = `CALL ${sp_nombre}(?,?,@RESULTADO)`;
     
-        connection.query(query, (error, resultado) => {
+        connection.query(query,[sp_parametro1,sp_parametro2], (error, resultado) => {
             if (error) {
                 console.error('Error al llamar al procedimiento almacenado:', error.message);
                 return res.status(500).json({ error: 'Error interno del servidor' });
             }
     
-            if (resultado && resultado.length > 0) {
-                res.json(resultado[0]);
-            } else {
-                res.json({ mensaje: 'No se encontraron resultados' });
+           // Fetch the output parameter value by executing another query
+           connection.query('SELECT @RESULTADO as outputValue', (err, result) => {
+            if (err) {
+                console.error('Error fetching output parameter:', err.stack);
+                return res.status(500).send('Error fetching data from the database');
             }
+            
+            if (result && result.length > 0) {
+                res.json(result[0]);
+            } else {
+                res.json({ mensaje: 'Ocurrió un error, no se puede iniciar sesión, intente nuevamente' });
+            } 
+            });
         });
     });
 
+
     app.post('/ResetPassword', (req, res) => {
         const sp_nombre = 'RESET_PASS';
-        const sp_parametro = req.body.id; // Obtiene el valor del parámetro 'id' del cuerpo de la solicitud
+         const sp_parametro1 = req.body.USUARIO;
+        const sp_parametro2 = req.body.CONTRASENIA; // Obtiene el valor del parámetro 'id' del cuerpo de la solicitud
     
         // Llama al procedimiento almacenado con el parámetro 'id'
-        const query = `CALL ${sp_nombre}(${sp_parametro})`;
+        const query = `CALL ${sp_nombre}(?,?,@RESULTADO)`;
     
-        connection.query(query, (error, resultado) => {
+        connection.query(query,[sp_parametro1,sp_parametro2], (error, resultado) => {
             if (error) {
                 console.error('Error al llamar al procedimiento almacenado:', error.message);
                 return res.status(500).json({ error: 'Error interno del servidor' });
             }
     
-            if (resultado && resultado.length > 0) {
-                res.json(resultado[0]);
-            } else {
-                res.json({ mensaje: 'No se encontraron resultados' });
+           // Fetch the output parameter value by executing another query
+           connection.query('SELECT @RESULTADO as outputValue', (err, result) => {
+            if (err) {
+                console.error('Error fetching output parameter:', err.stack);
+                return res.status(500).send('Error fetching data from the database');
             }
+            
+            if (result && result.length > 0) {
+                res.json(result[0]);
+            } else {
+                res.json({ mensaje: 'Ocurrió un error, no se puede resetear la clave, intente nuevamente' });
+            } 
+            });
         });
     });
 
